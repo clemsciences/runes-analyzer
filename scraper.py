18849e3c355582682fc0f27a6262721d61ccacf6
clement <clement@clement-debian>
2018-07-05 17:52:20 +0200

    scraping runes.verbix.com/index.html

--- scraper.py
+++ scraper.py
@@ -1,3 +1,7 @@
+import time
+import json
+from lxml import html
+
 
 
 site = "http://christerhamp.se/runor/gamla/index.html"
@@ -6,6 +10,26 @@ site = "http://christerhamp.se/runor/gamla/index.html"
 # extract the transliterated inscriptions : p[class="trans"]
 # extract the region of the inscription
 
-#
+# ----------------------------------------------------------
+site = "http://runes.verbix.com/index.html"
 
-#
+import requests
+
+
+rrunes = requests.get(site)
+tree = html.fromstring(rrunes.text)
+links = [i.get("href") for i in tree.xpath("//a") if "vikingage" in i.get("href")]
+runes = {}
+for link in links:
+    page = requests.get("http://runes.verbix.com/"+link)
+    tree = html.fromstring(page.text)
+    rune_blocks = tree.xpath('//p[@class="rrune"]')
+    print(len(rune_blocks))
+    content = [rb.text for rb in rune_blocks]
+    runes[link] = content
+    print(content)
+    time.sleep(1)
+with open("sweden_runes.json", "w") as f:
+    json.dump(runes, f)
+
+
