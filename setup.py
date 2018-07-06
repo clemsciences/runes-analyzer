e385f3ea2974540a5c1c5de4b6ed2835ad9ff159
clemsciences <clemsciences@aol.com>
2018-07-07 00:05:15 +0200

    into a module! (My first module!)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='runes-analyzer',
+    version='1.0.1',
+    packages=['runesanalyzer'],
+    url='',
+    license='',
+    author='Clément Besnier',
+    author_email='clemsciences@gmail.com',
+    description='Gathers different kinds of runic inscriptions'
+)
