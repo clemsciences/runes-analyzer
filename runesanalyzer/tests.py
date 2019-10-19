c391b42294e5b5e2cad2179d915d58cd8b64028f
Clément Besnier <clemsciences@aol.com>
2019-10-20 00:53:39 +0200

    Fixed tests

--- runesanalyzer/tests.py
+++ runesanalyzer/tests.py
@@ -10,4 +10,5 @@ __author__ = ["Clément Besnier <clemsciences@aol.com>", ]
 
 class Tests(unittest.TestCase):
     def test_elder_futhark(self):
-        self.assertListEqual([], data.ELDER_FUTHARK)
+        self.assertListEqual(['ᚠ', 'ᚢ', 'ᚦ', 'ᚨ', 'ᚱ', 'ᚲ', 'ᚷ', 'ᚹ', 'ᚺ', 'ᚾ', 'ᛁ', 'ᛃ', 'ᛇ,' 'ᛈ', 'ᛉ', 'ᛊ', 'ᛏ', 'ᛒ',
+                              'ᛖ', 'ᛗ', 'ᛚ', 'ᛜ', 'ᛟ', 'ᛞ'], data.ELDER_FUTHARK)
